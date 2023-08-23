31286682f58890654a8f2d7c95a723bbd84ec7fa
Guy Cherubin <ngchrbn@gmail.com>
2023-08-23 22:07:58 +0200

    Add initial files

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * _printf - Function that produces output according to a format.
+ * @format: character string, composed of zero or more directives.
+ * Return: number of characters printed,
+ * (excluding the null byte used to end ouput to strings)
+*/
+
+int _printf(const char *format, ...)
+{
+	return (0);
+}
